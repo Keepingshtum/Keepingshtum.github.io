b8a83f930f436a75bbea334999443bf6b2ae9b09
Anant Shukla <anant.shukla16@gmail.com>
2024-09-02 10:21:28 -0700

    Mo Markdown Mo Problems

--- src/components/BlogPostCard.tsx
+++ src/components/BlogPostCard.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import ReactMarkdown from 'react-markdown';
+import Markdown from 'react-markdown';
+import remarkGfm from 'remark-gfm'
 
 interface BlogPostCardProps {
   title: string;
@@ -10,7 +11,7 @@ const BlogPostCard: React.FC<BlogPostCardProps> = ({ title, content }) => {
   return (
     <div className="border rounded-lg shadow-md p-4 mb-4 bg-white dark:bg-gray-800">
       <h2 className="text-xl font-bold mb-2">{title}</h2>
-      <ReactMarkdown>{content}</ReactMarkdown>
+      <Markdown className="prose" remarkPlugins={[remarkGfm]}>{content}</Markdown>
     </div>
   );
 };
--- src/content/post4.md
+++ src/content/post4.md
@@ -1,4 +1,5 @@
 ### My Fourth Post
 
-This is a sample blog post written in Markdown.
-The markdown is definitely not rendering right now though!
+- This is a sample blog post written in Markdown.
+- The markdown is definitely not rendering right now though!
+- This is a link to this blog https://keepingshtum.github.io/
